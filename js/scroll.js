6d837c106351aa05a53d29b37c558a76e6fed1fb
cheikhsadbouh <cheikhsaadbouh24@gmail.com>
2017-10-18 00:02:36 +0000

    Initial commit

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+    $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+            $(".navbar-dark").css("background-color", "#00abcb"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        } else {
+            $(".navbar-dark").css("background-color", "transparent"); // if not, change it back to transparent
+        }
+    });
+});
+
+
+//slide carisoul 1000= 1 second = 1000 * 60 = 1 minute
+$(function () {
+    $('#carouselExampleIndicators').carousel({
+        interval: 1000 * 60
+
+    }).bind('slide', function () {
+        console.log("ended video ");
+
+    });
+});
